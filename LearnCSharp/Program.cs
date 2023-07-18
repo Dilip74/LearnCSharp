5f11f9bc16f587518aacae7c9c019b8c19556daf
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-07-18 19:07:16 +0530

    commit

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace LearnCSharp
 {
@@ -10,6 +8,27 @@ namespace LearnCSharp
     {
         static void Main(string[] args)
         {
+            int myNum = 15;
+            string myString = "Dilip";
+
+            Console.WriteLine("myNum Before : {0}", myNum);
+            ProcessNumber(myNum);
+            Console.WriteLine("myNum After : {0}", myNum);
+            Console.WriteLine("myString Before : {0}", myString);
+            ProcessString(myString);
+            Console.WriteLine("myString After : {0}", myString);
+
+            Console.ReadKey();
         }
-    }
+
+        public static void ProcessNumber(int num)
+        {
+            num = 100;
+        }
+
+        public static void ProcessString(string str)
+        {
+            str += " Kumar";
+        }
+    }    
 }
