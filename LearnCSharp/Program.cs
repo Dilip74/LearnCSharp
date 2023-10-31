2ff61832ef41825cf78a3fe9a41d1d5b6bb1ab19
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-10-31 18:16:58 +0530

    swapnumber

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -7,15 +7,15 @@ namespace LearnCSharp
         
         static void Main(string[] args)
         {
-            getData();
+            //getData();
+
+            //Swap Number without temp variable
+            /*
+            SwapNumber sn = new SwapNumber();
+            sn.SwapWithoutTemp(4, 5);
+            */
 
 
-            int a = 4, b = 5;
-            Console.WriteLine("Before swap a= " + a + " b= " + b);
-            a = a * b; //a=20 (4*5)      
-            b = a / b; //b=4 (20/5)      
-            a = a / b; //a=5 (20/4)    
-            Console.Write("After swap a= " + a + " b= " + b);
 
             Console.ReadKey();
         } 
---
+++ LearnCSharp/SwapNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    class SwapNumber
+    {
+        public void SwapWithoutTemp(int var1, int var2)
+        {
+            int a = var1, b = var2;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b; //a=20 (4*5)      
+            b = a / b; //b=4 (20/5)      
+            a = a / b; //a=5 (20/4)    
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
