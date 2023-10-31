227381f8682015950d191b171b3daa40d8c77883
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-10-31 11:36:37 +0530

    added web form project

---
+++ WebForms/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string connetionString;
+        SqlConnection conn;
+
+        //connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb ;User ID=sa;Password=demol23";
+
+        //conn = new SqlConnection(connetionString);
+
+        //conn.Open();
+
+        Response.Write("Connection MAde");
+        //conn.Close();
+    }
+}
