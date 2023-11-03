1b4a5d9887e0f5a1bccfcd539aa17d857ed3a45d
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-11-03 08:33:36 +0530

    added WPF

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -4,7 +4,8 @@ namespace LearnCSharp
 {
     class Program
     {
-        
+        delegate void SomeMethodPtr(int a, int b);
+
         static void Main(string[] args)
         {
             //getData();
@@ -14,12 +15,19 @@ namespace LearnCSharp
             SwapNumber sn = new SwapNumber();
             sn.SwapWithoutTemp(4, 5);
             */
-
+            int a = 4, b = 5;
+            SomeMethodPtr del = new SomeMethodPtr(SomeMethod);
+            del.Invoke(a, b);
 
 
             Console.ReadKey();
         } 
         
+        static void SomeMethod(int a, int b)
+        {
+            Console.WriteLine("Called some method");
+        }
+
         public static void getData()
         {
             ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
