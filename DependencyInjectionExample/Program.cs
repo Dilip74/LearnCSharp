7d5b0675b7c6e924ac1140868de6497f6a71ab0f
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-11-07 10:17:58 +0530

    a

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 
 namespace DependencyInjectionExample
 {
@@ -69,9 +70,150 @@ namespace DependencyInjectionExample
             */
 
 
+            //int[] A = { 1, 3, 6,7,8,9, 4, 1, 2 };
+            //int[] A = { -1, -3};
+            //Console.WriteLine(Solution(A));
 
+            //Geek g = new Geek();
+            //g.M1();
+            //g.M2();
+            //g.M3();
+            //g.M4();
+            //g.M5("Method Name: M5");
+
+            //NewMethodClass.M4(g);
+
+
+            
 
             Console.ReadKey();
-        }   
+        } 
+
+        static int Solution(int[] A)
+        {
+            int n = A.Length;
+            Array.Sort(A);
+            int res = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] == res)
+                    res++;
+
+                if (A[i] > res)
+                    return res;
+            }
+
+            return res;
+        }
+        
+    }
+
+    
+
+    class Base
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 Base");
+        }
+        public virtual void Method2()
+        { Console.WriteLine("Method2 Base");
+        }
+        public virtual void Method3()
+        { Console.WriteLine("Method3 Base");
+        }
+    }
+    class Derived : Base
+    {
+        new public void Method1()
+        {
+            Console.WriteLine("Method1 Derived");
+        }
+        public override void Method2()
+        {
+            Console.WriteLine("Method2 Derived");
+        }
+        public new void Method3()
+        {
+            Console.WriteLine("Method3 Derived");
+        }
+    }
+
+        class A
+    {
+        static A()
+        {
+
+        }
+    }
+
+    class Geek
+    {
+        // Method 1 
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        // Method 2 
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        // Method 3 
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+    }
+
+    static class NewMethodClass
+    {
+
+        // Method 4 
+        public static void M4(this Geek g)
+        {
+            Console.WriteLine("Method Name: M4");
+        }
+
+        // Method 5 
+        public static void M5(this Geek g, string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+
+
+    abstract class mcn
+    {
+        public int add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+    class mcn1 : mcn
+    {
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Area
+    {
+        public double area(double s)
+        {
+            double area = s * s;
+            return area;
+        }
+
+        public double area(double l, double b)
+        {
+            double area = l * b;
+            return area;
+        }
     }
 }
