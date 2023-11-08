2673c18715fe13c99fef76759c1aacdf8692b309
Dilip Kumar R <dilipkumarr04@gmail.com>
2023-11-08 16:56:37 +0530

    WinForms changes

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +9,27 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = Environment.OSVersion.ToString();
+            label2.Text = Environment.OSVersion.Platform.ToString();
+            label3.Text = Environment.MachineName.ToString();
+            label4.Text = Environment.ProcessorCount.ToString();
+
+            
+            //ulong ram = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
         }
     }
 }
